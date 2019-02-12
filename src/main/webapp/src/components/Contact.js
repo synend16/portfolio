c66106dcd2164f7b47fcbe3eac7b6177f084d798
synend16 <synend16@student.westerdals.no>
2019-02-12 23:16:41 +0100

    updated about me text on contact page

--- src/main/webapp/src/components/Contact.js
+++ src/main/webapp/src/components/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
+const imagesPath = process.env.PUBLIC_URL + "/assets/";
 
 
 class Contact extends Component {
@@ -8,13 +9,20 @@ class Contact extends Component {
 			<div className="contact-body">
 				<Grid className="contact-grid">
 					<Cell col={6}>
-						<h2>Paul Hanna</h2>
+						<h2>Endre M. Synnes</h2>
 						<img
-							src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+							src={`${imagesPath}avatar.jpg`}
 							alt="avatar"
-							style={{height: '250px'}}
+							style={{height: '200px'}}
 						/>
-						<p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+						<p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>
+							I am currently working as a Software developer at Aera Payment & Identification.
+							At the same time I'm finishing my bachelors degree in software development.
+							I will describe my self as a full stack developer, with the main focus on back-end development.
+							At work I have been developing web applications using SpringBoot with Java and front-end development
+							in React. At the same time I have learned other programming languages and frameworks at school such as
+							Kotlin, Angular, Ionic, Android development (in Java), C#, Swift, NodeJs, etc.
+						</p>
 					</Cell>
 					<Cell col={6}>
 						<h2>Contact Me</h2>
